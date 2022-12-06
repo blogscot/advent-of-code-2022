bdd37db4afea32f0b306733765246ce1c2758c48
blogscot <iain.diamond@gmail.com>
2022-12-06 09:55:57 +0000

    Major refactoring

--- day5/day5.lisp
+++ day5/day5.lisp
@@ -15,67 +15,50 @@
          (steps (subseq lines (1+ pos))))
     (list crates steps)))
 
-(defun crate-number (index) (1+ (floor index 4)))
-
 (defun get-stacks (crate-data)
-  (let ((stacks (loop for crate in (butlast crate-data)
-                      append (loop for ch across crate
-                                   for pos = 0 then (1+ pos)
-                                   when (alpha-char-p ch)
-                                     collect (cons (crate-number pos) ch)))))
-    (stable-sort stacks #'< :key #'car)))
+  (flet ((crate-number (index) (1+ (floor index 4))))
+    (loop for crate in (butlast crate-data)
+          append (loop for ch across crate
+                       for pos = 0 then (1+ pos)
+                       when (alpha-char-p ch)
+                         collect (cons (crate-number pos) ch)))))
 
 (defun get-steps (steps-data)
   (mapcar (lambda (step) (register-groups-bind (a b c)
                              ("move (\\d+) from (\\d+) to (\\d+)" step)
                            (mapcar #'parse-integer (list a b c)))) steps-data))
 
-(defun move-crate (stacks from to)
-  (let* ((old-crate (assoc from stacks))
-         (new-crate (cons to (cdr old-crate))))
-    (append (list new-crate) (remove old-crate stacks))))
+(defun move (stacks times from to &optional (f #'identity))
+  (let* ((old (remove from stacks :key 'car :count times))
+         (new (mapcar (lambda (crate) (cons to (cdr crate)))
+                      (set-difference stacks old))))
+    (append (funcall f new) old)))
 
-(defun move (stacks times from to)
-  (loop for n from 0 to times
-        for new-stacks = stacks then (move-crate new-stacks from to)
-        finally (return new-stacks)))
-
-(defun move-stacks (stacks steps)
+(defun move-stacks (stacks steps f)
   (reduce (lambda (stacks step)
             (destructuring-bind (times from to) step
-              (move stacks times from to)))
+              (funcall f stacks times from to)))
           steps :initial-value stacks))
 
-(defun get-solution-part1 (file)
+(defun get-solution (file f)
   (let* ((data (load-data file))
          (stacks (get-stacks (first data)))
          (steps (get-steps (second data)))
-         (moved (move-stacks stacks steps)))
+         (moved (move-stacks stacks steps f)))
     (map 'string (lambda (n) (cdr (assoc n moved)))
          (remove-duplicates (mapcar #'car stacks)))))
+
+(defun get-solution-part1 (file)
+  (get-solution file #'move))
 
 ;; (get-solution-part1 "day5.txt")
 ;; (get-solution-part1 "puzzle.txt")
 
-(defun move* (stacks times from to)
-  (let* ((old (remove from stacks :key 'car :count times))
-         (new (mapcar (lambda (crate) (cons to (cdr crate)))
-                      (set-difference stacks old))))
-    (stable-sort (append (reverse new) old) #'< :key #'car)))
-
-(defun move-stacks* (stacks steps)
-  (reduce (lambda (stacks step)
-            (destructuring-bind (times from to) step
-              (move* stacks times from to)))
-          steps :initial-value stacks))
+(defun move-with-crane (stacks times from to)
+  (move stacks times from to #'reverse))
 
 (defun get-solution-part2 (file)
-  (let* ((data (load-data file))
-         (stacks (get-stacks (first data)))
-         (steps (get-steps (second data)))
-         (moved (move-stacks* stacks steps)))
-    (map 'string (lambda (n) (cdr (assoc n moved)))
-         (remove-duplicates (mapcar #'car stacks)))))
+  (get-solution file #'move-with-crane))
 
 ;; (get-solution-part2 "day5.txt")
 ;; (get-solution-part2 "puzzle.txt")
